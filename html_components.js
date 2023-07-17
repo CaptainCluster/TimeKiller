33a0c9fb92cf62f5589cc9ce601d22b99fd05175
CaptainCluster <sranta.ville@gmail.com>
2023-07-17 15:37:17 +0300

    Added a new activity: Joke Generator

--- html_components.js
+++ html_components.js
@@ -12,30 +12,34 @@ function generateDefaultComponents(){
         {"Main Page" : "../index.html"},
         {"Activity Recommendator" : "../activity_recommendator/activityrecommendator.html"},
         {"Cat Facts" : "../cat_facts/catfacts.html"},
-        {"Dog Image Fetcher" : "../dog_image_generator/dogimagefetcher.html"}
+        {"Dog Image Fetcher" : "../dog_image_generator/dogimagefetcher.html"},
+        {"Joke Generator" : "../joke_generator/jokegenerator.html"}
     ]
     loadSelectionTab(activityNamesList);
 }
 
 function loadSelectionTab(activityNamesList){
     const selectionTabHolder = document.querySelector(".selectionTab"); //ul-tag
-    //Here we have a for-loop that generated the HTML page selector, allowing
+    //Here we have a forEach-loop that generates the HTML page selector, allowing
     //the user to select different activities.
     activityNamesList.forEach(pair => {
-        const key = Object.keys(pair)[0];
-        const value = pair[key];
+        forEachLoopProcess(selectionTabHolder, pair);
+    }); 
+}
 
+function forEachLoopProcess(selectionTabHolder, pair){
+    const key = Object.keys(pair)[0];
+    const value = pair[key];
     //We use a li-tag to display the entire selection, one line per activity
     //a-tags will provide user the hyperlink that they can click to move.
-        const linkHolder = document.createElement("li");
-        const selectableActivityElement = document.createElement("a");
+    const linkHolder = document.createElement("li");
+    const selectableActivityElement = document.createElement("a");
 
-        selectableActivityElement.href =  value;
-        selectableActivityElement.textContent = key + "\n"; 
+    selectableActivityElement.href =  value;
+    selectableActivityElement.textContent = key + "\n"; 
 
-        linkHolder.appendChild(selectableActivityElement);
-        selectionTabHolder.appendChild(linkHolder);
-    }); 
+    linkHolder.appendChild(selectableActivityElement);
+    selectionTabHolder.appendChild(linkHolder);
 }
 
 export {generateDefaultComponents};
---
+++ joke_generator/jokegenerator.js
@@ -0,0 +1,59 @@
+/** 
+@author CaptainCluster
+https://github.com/CaptainCluster
+*/
+import { generateDefaultComponents } from "../html_components.js";
+
+if(document.readyState !== "loading") {
+    mainFunction();
+} else {
+    document.addEventListener("DOMContentLoaded", function () {
+    mainFunction();
+    });
+}
+
+function mainFunction(){
+    generateDefaultComponents();
+    const jokeButton = document.getElementById("jokeButton");
+    jokeButton.addEventListener("click", function(){
+        jokeButtonEvent();
+    });
+}
+async function jokeButtonEvent(){
+    const rawData = await fetchData();
+    const processedData = await processData(rawData);
+    displayData(processedData);
+}
+
+async function fetchData(){
+    const url = "https://v2.jokeapi.dev/joke/Any?safe-mode";
+    const res = await fetch(url);
+    const rawData = await res.json();
+    return rawData;
+}
+
+async function processData(rawData){
+    //The jokes in the JSON file come in two types: single and twopart.
+    //The types have to be handled differently to avoid issues.
+    const jokeType = rawData.type;
+    let processedData = [];
+    if(jokeType == "single"){
+        processedData = [rawData.joke];
+    } else if(jokeType == "twopart"){
+        processedData = [rawData.setup, rawData.delivery]
+    }
+    return processedData;
+}
+
+function displayData(processedData){
+    const jokeElement = document.getElementById("joke");
+    jokeElement.textContent = ""; //We want to have one joke at a time
+    for(let i = 0; i < processedData.length; i++){
+        //Separating the parts of the twopart-type joke with a space
+        if(i != 0){
+            processedData[i] = " " + processedData[i];
+        }
+        //Here we have the part the user sees, being made during the for-loop
+        jokeElement.textContent = jokeElement.textContent + processedData[i];
+    }
+}
